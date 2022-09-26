5d066438aa7c66b1205d0b73b9ca9d65361c9205
Fabio Moro <fabio.moro2000@gmail.com>
2022-09-26 10:54:31 +0200

    Milestone 2

--- Program.cs
+++ Program.cs
@@ -1,105 +1,44 @@
 ﻿using System;
-public class Evento
+
+Console.WriteLine("inserisci il titolo del nuovo evento");
+string titolo = Console.ReadLine();
+
+Console.WriteLine("inserisci la data dell'evento");
+string data = Console.ReadLine();
+DateTime dataEvento = Convert.ToDateTime(data);
+
+Console.WriteLine("inserisci la capienza massima");
+int capienza = Convert.ToInt32(Console.ReadLine());
+
+Evento evento = new Evento(titolo, dataEvento, capienza);
+
+Console.WriteLine("quante prenotazioni vuoi effettuare?");
+
+int prenotazioni = Convert.ToInt32(Console.ReadLine());
+
+evento.PrenotaPosti(prenotazioni);
+
+Console.WriteLine("sono stati prenotati " + evento.PostiPrenotati + " posti");
+
+Console.WriteLine("vuoi disdire dei posti? (si/no)");
+string risposta = Console.ReadLine();
+
+if (risposta == "si")
 {
-    private string Titolo;
+    Console.WriteLine("quanti posti vuoi disdire?");
+    int postiDisdetti = Convert.ToInt32(Console.ReadLine());
 
-    private DateTime Data;
-
-    private int CapienzaMassima;
-    public int PostiPrenotati { get; set; }
-
-    public Evento(string titolo, DateTime data, int capienzaMassima)
-    {
-        SetTitolo(titolo);
-        SetData(data);
-        SetCapienzaMassima(capienzaMassima);
-        this.PostiPrenotati = 0;
-    }
-
-    public void PrenotaPosti(int posti)
-    {
-        DateTime thisDay = DateTime.Today;
-
-        if (PostiPrenotati < CapienzaMassima & CapienzaMassima > 0 & DateTime.Compare(thisDay, Data) < 0)
-        {
-            PostiPrenotati = PostiPrenotati + posti;
-        }
-        else
-        {
-            throw new Exception();
-        }
-    }
-
-    public void DisdiciPosti(int posti)
-    {
-        DateTime thisDay = DateTime.Today;
-
-        if (posti < PostiPrenotati & DateTime.Compare(thisDay, Data) < 0)
-        {
-            PostiPrenotati = PostiPrenotati - posti;
-        }
-        else
-        {
-            throw new Exception();
-        }
-    }
-
-    public string ToString()
-    {
-        string data = Data.ToString("dd/MM/yyyy");
-
-        return "data" + "-" + Titolo;
-    }
-    public string GetTitolo()
-    {
-        return Titolo;
-    }
-
-    public DateTime GetData()
-    {
-        return Data;
-    }
-
-    public int GetCapienzaMassima()
-    {
-        return CapienzaMassima;
-    }
-
-    public void SetTitolo(string titolo)
-    {
-        if (titolo != null & titolo != "")
-        {
-            Titolo = titolo;
-        }
-        else
-        {
-            throw new ArgumentNullException();
-        }
-    }
-
-    public void SetData(DateTime data)
-    {
-        DateTime thisDay = DateTime.Today;   
-        
-        if((DateTime.Compare(thisDay, data) < 0)) {
-            
-            Data = data;
-        }
-        else
-        {
-            throw new ArgumentException();
-        }
-    }
-
-    public void SetCapienzaMassima(int capienza)
-    {
-        if(capienza > 0)
-        {
-            CapienzaMassima = capienza;
-        }
-        else
-        {
-            throw new ArgumentException();
-        }
-    }
+    evento.DisdiciPosti(postiDisdetti);
 }
+
+Console.WriteLine("sono stati prenotati "+ evento.PostiPrenotati + " posti");
+
+
+
+
+
+
+
+
+
+
